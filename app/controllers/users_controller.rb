340f349d16ef34d29cf52a8672cda695604f325f
ltearz <s.avdonin94@gmail.com>
2020-09-02 01:07:11 +0300

    Added styles, question submit form and questions counter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 class UsersController < ApplicationController
-
-  def show
-    @hello = "Привет!"
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+        '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
   end
 
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.new(name: 'Sergey', username: 'ltearz', avatar_url: 'http://avki.ru/avatar-game-64x64/avki-ru-0128-avatar-game-64x64.gif')
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016'))
+    ]
+
+    @new_question = Question.new
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.gif'
+    end
+  end
+
+  def word_form(number, vopros, voprosa, voprosov)
+  remainder = number % 10
+  remainder1 = number % 100
+
+  if (11..14).include? remainder1
+    return voprosov
+  elsif remainder == 1
+    return vopros
+  elsif (2..4).include? remainder
+    return voprosa
+  else
+    return voprosov
+  end
+end
+
 end
